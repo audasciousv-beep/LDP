6ea98a9ae254a20d0b4cbc75f3d7a238b063e1e4
audasciousv-beep <audasciousv@gmail.com>
2026-06-24 20:18:17 -0300

    Create atv-reposicao.cpp

---
+++ atv-reposicao.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+printf("Número de argumentos: %d\n", argc);
+for (int i = 0; i < argc; i++) {
+printf("Argumento %d: %s\n", i, argv[i]);
+}
+return 0;
+}
