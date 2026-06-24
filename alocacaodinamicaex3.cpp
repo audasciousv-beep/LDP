8e72d7f50f055220d930002339fc30b3b8b494a8
audasciousv-beep <audasciousv@gmail.com>
2026-06-24 20:11:48 -0300

    Create alocacaodinamicaex3.cpp

---
+++ alocacaodinamicaex3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double a;
+int b;
+char c;
+string d;
+cout << sizeof(a) <<endl;
+cout << sizeof(b) <<endl;
+cout << sizeof(c) <<endl;
+cout << sizeof(d) <<endl;
+return 0;
+}
