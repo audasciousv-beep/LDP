ddd8037658ccc5c2f85dd4c67373d20aaa329b49
audasciousv-beep <audasciousv@gmail.com>
2026-06-24 19:49:56 -0300

    Create tipagem2.cpp

---
+++ tipagem2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char a = 'A';
+int b = 5;
+int soma = a + b;
+cout << "Letra original: " << a << endl;
+cout << "Valor do incremento: " << b << endl;
+cout << "Resultado final: " << soma << endl;
+return 0;
+}
