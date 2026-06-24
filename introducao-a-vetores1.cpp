2ff05147a8ca55476bdd4cb49fc4e361687570a5
audasciousv-beep <audasciousv@gmail.com>
2026-06-24 20:13:42 -0300

    Create introducao-a-vetores1.cpp

---
+++ introducao-a-vetores1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+vector<string> feira =
+{
+"Abacate", "Banana", "Brócolis", "Espinafre", "Maçã",
+"Morango", "Tomate", "Alface", "Batata", "Cenoura",
+"Melancia", "Uva", "Abacaxi", "Abóbora", "Chuchu"
+};
+cout << "Item na posicao 12: " << feira[11] << endl;
+return 0;
+}
