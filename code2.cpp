d5df598b8c82af37a99f70a705598ba1441c34ab
audasciousv-beep <audasciousv@gmail.com>
2026-06-24 19:19:00 -0300

    Create code2.cpp

---
+++ code2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+int main ()
+{
+string a;
+string b;
+string c;
+cout << "Qual seu nome? " << endl;
+cin >> a;
+cout << "Qual sua comida favorita? " << endl;
+cin >> b;
+cout << "Fale um hobby seu: " << endl;
+cin >> c;
+cout << "A comida favorita de " << a << " é " << b << " e seu hobby é " << c << endl ;
+return 0;
+}
